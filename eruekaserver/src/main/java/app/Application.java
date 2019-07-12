14fd050b99591b15c0b81e0a02fcd2c872814e6a
chenhy <410775541@qq.com>
2019-07-12 11:32:41 +0800

    eurake server add

--- eruekaserver/src/main/java/app/Application.java
+++ eruekaserver/src/main/java/app/Application.java
@@ -2,11 +2,9 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@EnableEurekaClient
 @ComponentScan(value = "controller")
 public class Application {
 
