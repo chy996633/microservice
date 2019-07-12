f671eeae47a2d665f0795e10fda7db683d90d63f
chenhy <410775541@qq.com>
2019-07-12 10:37:01 +0800

    first commit

---
+++ mircos2/src/main/java/controller/MainController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author <a herf="chenhy@itek-china.com">陈海越</a>
+ * @version 1.0
+ * @since 新标准版5.0
+ *
+ * <pre>
+ * 历史：
+ *      建立: 2019/7/12 陈海越
+ *        </pre>
+ */
+@Controller
+@RequestMapping("/s2")
+public class MainController {
+
+    @RequestMapping("/start")
+    public void start() {
+        System.out.println("service2 start");
+    }
+
+}
